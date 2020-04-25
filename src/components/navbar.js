ba4159f50994e8187256f4e185bd9b5f66826897
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-04-25 23:16:01 +0200

    navbar parly styled

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
+import { FaAlignRight, FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa"
 
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  outline: none;
+  margin: auto 0 auto auto;
+  &:hover {
+    cursor: pointer;
+  }
+  @media (min-width: 800px) {
+    display: none;
+  }
+`
+
+const SFaAlignRight = styled(FaAlignRight)`
+  color: var(--primary);
+  font-size: 1.5rem;
+`
 const Nav = styled.nav`
-  @media (min-width: 768px) {
+  display: ${p => (p.open ? "block" : "none")};
+  @media (min-width: 800px) {
     display: flex;
     background: none;
     left: initial;
@@ -11,21 +30,29 @@ const Nav = styled.nav`
     border-bottom: none;
     position: relative;
     width: initial;
+    margin: auto 0 auto auto;
   }
   a {
     padding: 4px 8px;
     display: block;
     text-align: center;
-    margin: auto 0;
+    color: var(--primary);
+    font-weight: bold;
   }
 `
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [isOpen, setMenu] = useState(false)
+  const toggleMenu = () => {
+    setMenu(isOpen => !isOpen)
+  }
 
   return (
     <header>
-      <Nav>
+      <Button type="button" onClick={toggleMenu}>
+        <SFaAlignRight />
+      </Button>
+      <Nav open={isOpen}>
         <Link to="/">Home</Link>
         <Link to="/page-2">Second Page</Link>
         <Link to="/contact">Contact</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Header from "../components/header"
+//import Header from "../components/header"
 
 const IndexPage = ({ children }) => {
   const data = useStaticQuery(graphql`
